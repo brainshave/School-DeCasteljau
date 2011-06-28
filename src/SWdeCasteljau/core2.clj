de0d701dd5c5e01f4b03850985dc832082c45351
Szymon Witamborski <santamon@gmail.com>
2011-06-28 23:35:56 +0200

    Changed to use the new Canvas class instead of custom PaintPanel.

--- src/SWdeCasteljau/core2.clj
+++ src/SWdeCasteljau/core2.clj
@@ -1,16 +1,17 @@
 (ns SWdeCasteljau.core2
   (:gen-class)
   (:use (guiftw swing events)
-	(SWdeCasteljau math PaintPanel)
-	clojure.pprint
+	(SWdeCasteljau math)
+	(clojure pprint)
 	(clojure.contrib (string :only (replace-str))))
-  (:import (javax.swing JLabel JFrame JSlider JToggleButton
-			SwingUtilities)
+  (:import (guiftw.swing Canvas CanvasListener)
+           (javax.swing JLabel JFrame JSlider JToggleButton
+                        SwingUtilities)
 	   (java.awt.event MouseListener MouseMotionListener MouseEvent
 			   ActionListener)
+           (java.awt Color RenderingHints)
 	   (javax.swing.event ChangeListener)
-	   SWdeCasteljau.PaintPanel
-	   net.miginfocom.swing.MigLayout))
+           net.miginfocom.swing.MigLayout))
 
 (def *initial-gui-state* {:animator (agent nil)})
 
@@ -102,6 +103,30 @@
     (Thread/sleep 16 666)
     (send (-> @gui :animator) t-step gui)))
 
+
+(defn paint-canvas [_ event]
+  (let [g (.getGraphics event)
+        widget (.getComponent event)]
+    (doto g
+      (.setRenderingHint RenderingHints/KEY_ANTIALIASING
+                         RenderingHints/VALUE_ANTIALIAS_ON)
+      (.setColor Color/WHITE)
+      (.fillRect 0 0 (.getWidth widget) (.getHeight widget))
+      (.setColor Color/GRAY))
+    (dorun (map #(dorun (map (fn [[x1 y1] [x2 y2]] (.drawLine g x1 y1 x2 y2))
+                             % (rest %)))
+                @*points*))
+    (.setColor g Color/BLACK)
+    (let [approxims @*curve*]
+      (dorun (map (fn [[x1 y1] [x2 y2]]
+                    (.drawLine g x1 y1 x2 y2))
+                  approxims (rest approxims))))
+    (let [[x y] (last (last @*points*))]
+      (.setColor g Color/BLACK)
+      (.drawOval g (- x 8) (- y 8) 16 16))
+    (.setColor g Color/RED)
+    (dorun (map (fn [[x y]] (.drawOval g (- x 5) (- y 5) 10 10)) (first @*points*)))))
+
 (defn toggle-animation [gui event]
   (let [animate  (.. event getSource isSelected)]
     (swap! gui #(assoc % :animate animate))
@@ -116,10 +141,11 @@
 						"0[top] [top] [top] [top] [top] [top] [top,grow]0")
 			    :size ^unroll (800 700)
 			    :visible true]
-		    [PaintPanel [:*id :paint-panel
-				 :*lay "span 1 7, grow"
-				 :mouse++pressed mouse-down
-				 :mouse-motion+mouse-dragged mouse-dragged]]
+		    [Canvas [:*id :paint-panel
+                             :*lay "span 1 7, grow"
+                             :canvas+paint paint-canvas
+                             :mouse++pressed mouse-down
+                             :mouse-motion+mouse-dragged mouse-dragged]]
 		    [JLabel [:text (str "<html><b>Obsługa:</b><br>Lewy klawisz: przesuwaj punkty<br>"
 					"Prawy klawisz: dodawaj/usuwaj punkty</html>")]]
 		    [JLabel [:*id :density-label :text "density"]]
